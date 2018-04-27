4ebe828ab7faa3781e6bbd38b4e8c3b25b5ed3dc
Clemilton Pereira <clemilton.ufam@gmail.com>
2018-04-26 23:27:28 -0400

    EJS - Add Views

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ var app= express();
 
 //Ajustando a view engine do express
 app.set('view engine','ejs');
+
 app.get('/',function(req,res){
 
-	res.render("<html><body>Portal de notícias</body></html>");
+	res.render("home/index");
 });
 
-app.get('/tecnologia',function(req,res){
-	res.render("secao/tecnologia");
+app.get('/formulario_inclusao_noticia',function(req,res){
+	res.render("admin/form_add_noticia");
+});
+
+app.get('/noticias',function(req,res){
+	res.render("noticias/noticias");
 });
 
 app.listen(3000,function(){
